56017ecd1757f42a28848fd4b742a0cb67cba5d4
fadelanumah <fadelanumah@gmail.com>
2024-07-16 23:05:41 +0700

    fix(notes): Change Index Background Color

--- resources/views/notes/index.blade.php
+++ resources/views/notes/index.blade.php
@@ -7,12 +7,10 @@
 
     <div class="py-12">
         <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-            <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    <x-button primary>Hi there Notes</x-button>
-                    <div class="mt-2">
-                        <livewire:notes.show-notes />
-                    </div>
+            <div class="p-6 text-gray-900">
+                <x-button primary>Hi there Notes</x-button>
+                <div class="mt-2">
+                    <livewire:notes.show-notes />
                 </div>
             </div>
         </div>
